bab7d40370e13c6a4da9fe96801f89805e1f0a7e
KGPonsekhaa <ponsekhagurusamy@gmail.com>
2026-05-26 19:31:04 +0530

    Semantic Retrieval Working Successfully with no gpt model

--- app/main.py
+++ app/main.py
@@ -15,9 +15,15 @@ def home():
     pdf_text = load_pdf("data/sample.pdf")
     chunks = create_chunks(pdf_text)
     vector_store = create_vector_store(chunks)
+    query = "What is the purpose of this document?"
 
+    docs = vector_store.similarity_search(query, k=3)
+    results = []
+
+    for doc in docs:
+        results.append(doc.page_content[:500])
 
     return {
-        "message": "FAISS Vector Store Created Successfully",
-        "total_chunks": len(chunks)
+        "message": "Semantic Retrieval Working Successfully",
+        "retrieved_chunks": results
     }
