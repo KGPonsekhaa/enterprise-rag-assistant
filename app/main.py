b90d5a4838f6ad7f931a1eb80ad74a1b049089aa
KGPonsekhaa <ponsekhagurusamy@gmail.com>
2026-05-26 20:00:21 +0530

    end-to-end enterprise-grade RAG assistant

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.pdf_loader import load_pdf
 from app.rag_pipeline import create_chunks
 from app.vector_store import create_vector_store
+from app.prompt_builder import generate_response
 
 app = FastAPI(
     title="Enterprise RAG Assistant",
@@ -9,21 +10,35 @@ app = FastAPI(
     version="1.0.0"
 )
 
-@app.get("/")
-def home():
+@app.get("/ask")
+def ask_question(question: str):
 
     pdf_text = load_pdf("data/sample.pdf")
     chunks = create_chunks(pdf_text)
     vector_store = create_vector_store(chunks)
-    query = "What is the purpose of this document?"
-
-    docs = vector_store.similarity_search(query, k=3)
-    results = []
-
-    for doc in docs:
-        results.append(doc.page_content[:500])
-
+    docs = vector_store.similarity_search(question, k=3)
+    context = "\n".join([doc.page_content for doc in docs])
+    answer = generate_response(context, question)
     return {
-        "message": "Semantic Retrieval Working Successfully",
-        "retrieved_chunks": results
+        "question": question,
+        "answer": answer
     }
+
+# @app.get("/")
+# def home():
+
+#     pdf_text = load_pdf("data/sample.pdf")
+#     chunks = create_chunks(pdf_text)
+#     vector_store = create_vector_store(chunks)
+#     query = "What is the purpose of this document?"
+
+#     docs = vector_store.similarity_search(query, k=3)
+#     results = []
+
+#     for doc in docs:
+#         results.append(doc.page_content[:500])
+
+#     return {
+#         "message": "Semantic Retrieval Working Successfully",
+#         "retrieved_chunks": results
+#     }
--- app/prompt_builder.py
+++ app/prompt_builder.py
@@ -1,1 +1,33 @@
 # Prompt engineering logic
+import os
+from dotenv import load_dotenv
+from langchain_openai import AzureChatOpenAI
+load_dotenv()
+
+llm = AzureChatOpenAI(
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    deployment_name=os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT"),
+    temperature=0
+)
+
+
+def generate_response(context, question):
+
+    prompt = f"""
+    You are an enterprise AI assistant.
+    Answer ONLY from the provided context.
+    If answer is not available in context,
+    say:
+    'Answer not found in provided documents.'
+
+    Context:
+    {context}
+
+    Question:
+    {question}
+    """
+
+    response = llm.invoke(prompt)
+    return response.content
